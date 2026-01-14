793eb2b5d235999b82ccd98999c20c78b6620008
Documenter.jl <documenter@juliadocs.github.io>
2026-01-14 04:51:46 +0000

    build based on 9662b42

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MagicEntanglement","page":"Home","title":"MagicEntanglement","text":"Documentation for MagicEntanglement.\n\n","category":"section"}]
+}
